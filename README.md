bddd424ca2e2d4acb9a3a1f002c736cccb5e2e89
juanspdf <132899302+JuanPasquel@users.noreply.github.com>
2025-10-22 12:23:53 -0500

    Cliente acabado

---
+++ README.md
@@ -0,0 +1,28 @@
+# Descripción del laboratorio
+
+# Integrantes del grupo
+Juan David Pasquel Ruiz
+
+
+## Servidor sin hilos
+
+## Servidor con hilos
+
+## Cliente
+
+# Estructura de directorios
+
+laboratorio2/
+|
+├── con_hilos/
+│   ├── calificaciones.csv      //
+│   ├── client.py               //
+│   └── server.py               //
+├── sin_hilos/
+│   ├── calificaciones.csv      //Archivo
+│   ├── client.py               //
+│   └── server.py               //
+├── nrcs_server.py              //
+└── nrcs.csv
+
+# Instrucciones de ejecución
--- sin_hilos/client.py
+++ sin_hilos/client.py
@@ -2,14 +2,14 @@ import socket
 import json
 
 def mostrar_menu():
-    print(\n--- Men de Calificaciones ---)
-print(1. Agregar calificacin)
-    print(2. Buscar por ID)
-    print(3. Actualizar calificacin)
-    print(4. Listar todas)
-print(5. Eliminar por ID)
-    print(6. Salir)
-    return input(Elija opcin: )
+    print('\n--- Menu de Calificaciones ---')
+    print('1. Agregar calificacion')
+    print('2. Buscar por ID')
+    print('3. Actualizar calificacion')
+    print('4. Listar todas')
+    print('5. Eliminar por ID')
+    print('6. Salir')
+    return input('Elija opcion:')
 
 def enviar_comando(comando):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,3 +18,40 @@ def enviar_comando(comando):
     respuesta = client_socket.recv(1024).decode('utf-8')
     client_socket.close()
     return json.loads(respuesta)
+
+while True:
+    opcion = mostrar_menu()
+    if opcion == '1':
+        id_est = input('ID: ')
+        nombre = input('Nombre: ')
+        materia = input('Materia: ')
+        calif = input('Calificacion: ')
+        res = enviar_comando(f'AGREGAR|{id_est}|{nombre}|{materia}|{calif}')
+        print(res['mensaje'])
+    elif opcion == '2':
+        id_est = input('ID: ')
+        res = enviar_comando(f'BUSCAR|{id_est}')
+        if res['status'] == 'ok':
+            print(f'Nombre: {res['data']['Nombre']}, Materia: {res['data']['Materia']}, Calificacion: {res['data']['Calificacion']}')
+        else:
+            print(res['mensaje'])
+    elif opcion == '3':
+        id_est = input('ID: ')
+        nueva_calif = input('Nueva calificacion: ')
+        res = enviar_comando(f'ACTUALIZAR|{id_est}|{nueva_calif}')
+        print(res['mensaje'])
+    elif opcion == '4':
+        res = enviar_comando('LISTAR')
+        if res['status'] == 'ok':
+            for row in res['data']:
+                print(row)
+        else:
+            print(res['mensaje'])
+    elif opcion == '5':
+        id_est = input('ID: ')
+        res = enviar_comando(f'ELIMINAR|{id_est}')
+        print(res['mensaje'])
+    elif opcion == '6':
+        break
+    else:
+        print('Opcion invalida')
